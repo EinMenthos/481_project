6abad7d2a7dfe0454e808a3f2c5b8b0d1974bf08
Pedro Morales <pedrojmorales01@gmail.com>
2023-11-12 21:03:30 -0800

    game board can now be initialized with n rows and n columns

--- game/board.py
+++ game/board.py
@@ -8,8 +8,25 @@ class Board:
         self._screen = screen
         self._rows = rows
         self._cols = cols
-        self._rect = pygame.Rect(self.screen_width * 0.125, 150, self.screen_width * 0.75, self.screen_width * 0.75)
-        self._disks = [Disk(self._screen, "red", (400, 400), 50)]
+        self._rect = pygame.Rect(int(self.screen_width * 0.125), int(self.screen_height * 0.25), int(self.screen_width * 0.75), int(self.screen_height * 0.75))
+        self._disk_width = int((self._rect.width / cols) * 0.75) # width of 1 disk
+        self._disk_height = int((self._rect.height / rows) * 0.75) # height of 1 disk
+        self._disk_diameter = min(self._disk_width, self._disk_height) # pick the smallest between width and height to set as diameter
+        self._disk_radius =  self._disk_diameter / 2 # set the radius
+        self._gap_x = (self._rect.width - (self._disk_diameter * cols)) // (cols + 1) # set the gap between columns
+        self._gap_y = (self._rect.height - (self._disk_diameter * rows)) // (rows + 1) # set the gap between rows
+        # create the disks
+        self._disks = [
+            [Disk(
+                screen,
+                "white", 
+                (
+                    self._rect.left + ((j + 1) * self._gap_x) + (self._disk_diameter * j) + self._disk_radius,
+                    self._rect.top + ((i + 1) * self._gap_y) + (self._disk_diameter * i) + self._disk_radius
+                ),
+                self._disk_width
+            ) 
+            for j in range(self._cols)] for i in range(self._rows)]
     @property
     def rect(self):
         """Returns the bounding rect"""
@@ -24,7 +41,7 @@ class Board:
     def screen_height(self):
         """Returns the screen's height"""
         return pygame.Surface.get_height(self._screen)
-
+    
     @property
     def disks(self):
         """Return list of disks"""
@@ -33,8 +50,8 @@ class Board:
     def draw(self):
         """Draws the game board and disks"""
         pygame.draw.rect(self._screen, (45,92,214), self.rect, border_radius=5)
-        for disk in self.disks:
-            disk.draw() 
-
+        for i, row in enumerate(self.disks):
+            for j, disk in enumerate(row):
+                disk.draw() 
     def process_events(self, event):
         """Process the game's events"""
--- game/disk.py
+++ game/disk.py
@@ -4,7 +4,8 @@ class Disk(pygame.sprite.Sprite):
     """Creates a disk game piece"""
     # save and load the images
     _images = {'red': pygame.image.load('game/red_disk.bmp'), 
-        'yellow': pygame.image.load('game/yellow_disk.bmp')}
+        'yellow': pygame.image.load('game/yellow_disk.bmp'),
+        'white': pygame.image.load('game/white_disk.bmp')}
 
     def __init__(self, screen, color, center, width):
         """Initialize the disk based on color"""
@@ -13,17 +14,22 @@ class Disk(pygame.sprite.Sprite):
         self._color = color
         self._center = center
         self._width = width
-        # set the sprite
-        self._sprite = Disk._images[color]
+        # create the sprite
+        self._sprite = pygame.transform.scale(Disk._images[color], (width, width))
         # get the bounding rect of the sprite
         self._rect = self._sprite.get_rect()
-        self._rect.topleft = center
+        self._rect.center = center # set the center
 
     @property
     def rect(self):
         """Returns bounding rect"""
         return self._rect
     
+    @property
+    def center(self):
+        """Returns the sprite's center"""
+        return self._center
+    
     def draw(self):
         """Draw the sprite"""
-        self._screen.blit(self._sprite, self.rect.topleft)
+        self._screen.blit(self._sprite, self.rect)
